9aaecb2480c11293fd9b4184a11c53bab28ef643
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-06-30 17:15:54 +0700

    client & contact me done

--- Portfolio-V2/script.js
+++ Portfolio-V2/script.js
@@ -18,3 +18,45 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('hidden');
 
 });
+
+const buttonCloseNotif = document.querySelector("#close-notif-berhasil");
+const tulisanBerhasil = document.querySelector("#notif-berhasil");
+const buttonKirim = document.querySelector(".btn-kirim");
+//const buttonTunggu = document.querySelector(".btn-tunggu");
+
+buttonCloseNotif.addEventListener('click', function () {
+    tulisanBerhasil.classList.toggle("hidden");
+});
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbwqljeJBO2twb5X9P14CytVsTtGhG-RsCBofA27O0kQjD9Popj-xFFGBZoHFAs_VPxJ/exec";
+const form = document.forms["contact-form"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    buttonKirim.classList.toggle("hidden");
+    //buttonTunggu.classList.toggle("hidden");
+    fetch(scriptURL, {
+            method: "POST",
+            body: new FormData(form)
+        })
+        .then((response) => {
+            console.log("Success!", response);
+            buttonKirim.classList.toggle("hidden");
+            //buttonTunggu.classList.toggle("hidden");
+            tulisanBerhasil.classList.toggle("hidden");
+            form.reset();
+        })
+        .catch((error) => console.error("Error!", error.message));
+});
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// const navBar = document.querySelector(".navbar-nav");
+
+// navBar.addEventListener("click", function (e) {
+//     navLinks.forEach((i) => {
+//         i.classList.remove("active");
+//         if (i.className === e.target.className) {
+//             e.target.classList.add("active");
+//         }
+//     });
+// });
